ebbff03e67c469ac36d370eb3a9ae85f9d97c7f6
Björn Büttner <bjoern.buettner@jtl-software.com>
2023-07-01 17:21:01 +0200

    make descriptions longer fix typo in prices fix typo in imprint-en

--- src/Pages/Prices.php
+++ src/Pages/Prices.php
@@ -14,9 +14,10 @@ class Prices
                 return $twig->render('prices.twig', [
                     'title' => 'Offers',
                     'active' => '/prices',
-                    'description' => 'Offers by Björn Büttner',
+                    'description' => 'Offers by Björn Büttner in a well sorted overview',
                     'content' => [
                         'title' => 'My offers and prices',
+                        'title2' => 'Offer details',
                         'reasoning' => 'The prices are primarily based on my interest and the relative effort of the tasks. Individual prices will be provided on request.',
                         'prices' => 'All prices include taxes and assume remote work. Contract termination for monthly offers is, if not agreed on differently, is 30 days until the end of the month.',
                     ],
@@ -27,11 +28,12 @@ class Prices
                 return $twig->render('prices.twig', [
                     'title' => 'Leistungen',
                     'active' => '/prices',
-                    'description' => 'Leistungen von Björn Büttner',
+                    'description' => 'Leistungen von Björn Büttner in einem gut sortierten Überblick',
                     'content' => [
                         'title' => 'Meine Leistungen & Preise',
+                        'title2' => 'Leistungsdetails',
                         'reasoning' => 'Die Preise orientieren sich primär an meinem Interesse und dem relativen Aufwand der Tätigkeiten. Individuelle Angebote erhalten Sie auf Anfrage.',
-                        'prices' => 'Alle Preise verstehen sich inklusive Steuern und setzen Remotearbeit vorraus. Kündigungsfrist für monatliche Angebote ist, falls nicht anders abgesprochen, 30 Tage zum Monatsende.',
+                        'prices' => 'Alle Preise verstehen sich inklusive Steuern und setzen Remotearbeit voraus. Kündigungsfrist für monatliche Angebote ist, falls nicht anders abgesprochen, 30 Tage zum Monatsende.',
                     ],
                     'offers' => OfferList::get(),
                 ]);
