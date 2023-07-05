fbd06493aa46c5676d5a5fbe7d92e1f964bc0005
Björn Büttner <bjoern.buettner@jtl-software.com>
2023-07-05 15:16:53 +0200

    add start command to composer json add cache to twig wrapper

--- src/TwigWrapper.php
+++ src/TwigWrapper.php
@@ -24,23 +24,40 @@ class TwigWrapper extends Environment
         }, ['is_safe' => ['html']]));
     }
 
+    private function getCacheKey(string $template, array $context): string
+    {
+        return $this->lang . md5(filemtime($template)) . md5($template) . sha1(json_encode($context));
+    }
+
     public function render($template, array $context = []): string
     {
+        $cache = dirname(__DIR__) . '/cache/' . $this->getCacheKey($template, $context) . '.min.twig';
+        if (is_file($cache)) {
+            return file_get_contents($cache);
+        }
         $data = $this->renderUnminified($template, $context);
         try {
             $htmlMin = new HtmlMin();
-            return $htmlMin->minify($data);
+            $data = $htmlMin->minify($data);
         } catch (Exception $e) {
-            return $data;
+            // ignore
         }
+        file_get_contents($cache, $data);
+        return $data;
     }
 
     public function renderUnminified(string $template, array $context = []): string
     {
+        $cache = dirname(__DIR__) . '/cache/' . $this->getCacheKey($template, $context) . '.twig';
+        if (is_file($cache)) {
+            return file_get_contents($cache);
+        }
         $context['lang'] = $this->lang;
         $context['menu'] = $this->lang === 'en' ? MenuList::$en : MenuList::$de;
         $context['og_type'] = $context['og_type'] ?? 'website';
         $context['author'] = $context['author'] ?? ['name' => 'Björn Büttner'];
-        return parent::render($template, $context);
+        $data = parent::render($template, $context);
+        file_put_contents($cache, $data);
+        return $data;
     }
 }
