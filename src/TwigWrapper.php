d4ad220ba7ca8f0fe172e488a4f6b4ee92340afb
Björn Büttner <bjoern.buettner@jtl-software.com>
2023-07-06 12:53:25 +0200

    formatting fixes

--- src/TwigWrapper.php
+++ src/TwigWrapper.php
@@ -29,7 +29,10 @@ class TwigWrapper extends Environment
 
     private function getCacheKey(string $template, array $context): string
     {
-        return $this->lang . md5((string) filemtime(dirname(__DIR__) . '/templates/' . $template)) . md5($template) . sha1(json_encode($context));
+        return $this->lang
+            . md5((string) filemtime(dirname(__DIR__) . '/templates/' . $template))
+            . md5($template)
+            . sha1(json_encode($context));
     }
 
     public function render($template, array $context = []): string
