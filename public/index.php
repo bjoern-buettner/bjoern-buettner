360d7e65a31bf1acffff00bd00610941afd84c1a
Björn Büttner <bjoern.buettner@jtl-software.com>
2023-07-14 13:51:50 +0200

    add strict types

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Dotenv\Dotenv;
 use Me\BjoernBuettner\Application;
 use Me\BjoernBuettner\Cache;
--- src/User.php
+++ src/User.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Me\BjoernBuettner;
 
 interface User
