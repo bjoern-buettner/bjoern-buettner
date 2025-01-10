1a63d45cc18a4151375efa993f210ee2118a60d3
Björn Büttner <Idrinth@users.noreply.github.com>
2025-01-10 15:35:03 +0100

    fix cookie notification

--- resources/menu-hover.js
+++ resources/menu-hover.js
@@ -2,7 +2,7 @@
     const els = document.getElementById('header').firstElementChild.getElementsByTagName('ul');
     for (let i = 0; i < els.length; i++) {
         const li = els[i].parentElement;
-        li.bind('mouseenter', (e) => {
+        li.addEventListener('mouseenter', (e) => {
             li.lastElementChild.style.left = li.offsetLeft + 'px';
         })
     }
